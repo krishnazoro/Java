632469f78acce8f2e15ee82c0d554cd2498157a6
krishnazoro <147299012+krishnazoro@users.noreply.github.com>
2026-09-07 23:06:20 +0530

    data

--- firstpr/src/firstpr/DataTypes.java
+++ firstpr/src/firstpr/DataTypes.java
@@ -5,7 +5,24 @@ public class DataTypes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+		int age = 24;
+		float b = 32.6f;
+		double mark = 55.66;
+		boolean presence = true;
+		char level = 'A';
+		String name = "krishna";
+		long value = 800000000l;
+		
+		System.out.println(age);
+		System.out.println(b);
+		System.out.println(mark);
+		System.out.println(presence);
+		System.out.println(level);
+		System.out.println(name);
+		System.out.println(value);
+		
+		
+		
 	}
 
 }
